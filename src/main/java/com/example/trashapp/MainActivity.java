04683a679656132fc56e5d0f99b6ef8c26dd41d3
da-ha3ker <daha3ker@users.noreply.github.com>
2019-02-25 09:35:29 -0700

    added a test comment

--- src/main/java/com/example/trashapp/MainActivity.java
+++ src/main/java/com/example/trashapp/MainActivity.java
@@ -30,7 +30,7 @@ public class MainActivity extends AppCompatActivity {
             return false;
         }
     };
-
+ //HELOO AWERAWSERASGAFGATHAHSGABEWYWEH RHR TFH F
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
