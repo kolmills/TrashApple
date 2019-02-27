b750a2fca2e51e927b4680062d0493b979035c5f
Kolton <kolton.mills93@gmail.com>
2019-02-27 00:48:21 -0700

    added data structures with getters and setters. also added some void functions

--- src/main/java/com/example/trashapp/BackgroundWorker.java
+++ src/main/java/com/example/trashapp/BackgroundWorker.java
@@ -4,5 +4,59 @@ public class BackgroundWorker{
     private Employee employeeObject;
     private Map mapObject;
     private Customer customerObject;
+    private List mapList;
+    private List ticketList;
+    private List customerList;
 
+    public Employee getEmployeeObject() {
+        return employeeObject;
+    }
+
+    public void setEmployeeObject(Employee employeeObject) {
+        this.employeeObject = employeeObject;
+    }
+
+    public Map getMapObject() {
+        return mapObject;
+    }
+
+    public void setMapObject(Map mapObject) {
+        this.mapObject = mapObject;
+    }
+
+    public Customer getCustomerObject() {
+        return customerObject;
+    }
+
+    public void setCustomerObject(Customer customerObject) {
+        this.customerObject = customerObject;
+    }
+
+    public List getMapList() {
+        return mapList;
+    }
+
+    public void setMapList(List mapList) {
+        this.mapList = mapList;
+    }
+
+    public List getTicketList() {
+        return ticketList;
+    }
+
+    public void setTicketList(List ticketList) {
+        this.ticketList = ticketList;
+    }
+
+    public List getCustomerList() {
+        return customerList;
+    }
+
+    public void setCustomerList(List customerList) {
+        this.customerList = customerList;
+    }
+
+    public void UpdateJsonStorage(){
+
+    }
 }
