9e903fa67bedbb5a4d65938fae0f15599e259f5f
da-ha3ker <daha3ker@users.noreply.github.com>
2019-03-15 09:17:39 -0600

    detailactivity gone

--- src/main/java/com/example/trashapp/BackgroundWorker.java
+++ src/main/java/com/example/trashapp/BackgroundWorker.java
@@ -1,6 +1,20 @@
 package com.example.trashapp;
 
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.util.Log;
+
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
+import com.google.firebase.database.ValueEventListener;
+
 import java.util.List;
+
+import static android.support.constraint.Constraints.TAG;
 
 public class BackgroundWorker{
     private Employee employeeObject;
@@ -11,11 +25,8 @@ public class BackgroundWorker{
     private List<Map> mapList;
     private List<Ticket> ticketList;
     private List<Customer> customerList;
-//ytfujh
-    ///We haveeeeeeeeee beeeeeeeeeeee
-    //There it goes ese fseg rdgdlkgjsd;lgjl;kfjgb;srijg;sdrkjg;srdijgv;dsomfbgnd;ojfdlk
-    //I wannnnnnnnnnnnnnnnnnnnnnannnnnnnnnngfodgnkdl fgn
-    //er lgelrjgostigjpkoprskogp;seor
+    FirebaseDatabase database = FirebaseDatabase.getInstance();
+    DatabaseReference myRef = database.getReference("Customer");
 
     public Employee getEmployeeObject() {
         return employeeObject;
@@ -35,10 +46,12 @@ public class BackgroundWorker{
 
     public Customer getCustomerObject() {
         return customerObject;
+
     }
 
     public void setCustomerObject(Customer customerObject) {
         this.customerObject = customerObject;
+        myRef.child("Customers").setValue(customerObject);
     }
 
     public List getMapList() {
@@ -50,6 +63,22 @@ public class BackgroundWorker{
     }
 
     public List getTicketList() {
+
+        myRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                // This method is called once with the initial value and again
+                // whenever data at this location is updated.
+                String value = dataSnapshot.getValue(String.class);
+                Log.d(TAG, "Value is: " + value);
+            }
+
+            @Override
+            public void onCancelled(DatabaseError error) {
+                // Failed to read value
+                Log.w(TAG, "Failed to read value.", error.toException());
+            }
+        });
         return ticketList;
     }
 
@@ -58,15 +87,53 @@ public class BackgroundWorker{
     }
 
     public List getCustomerList() {
+        myRef.child("Customers").getDatabase();
+
+                String myUserId = "Customers";
+        Query myTopPostsQuery = myRef.child("Customers").child(myUserId)
+                .orderByChild("");
+        myTopPostsQuery.addChildEventListener(new ChildEventListener() {
+            @Override
+            public void onChildAdded(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
+
+            }
+
+            @Override
+            public void onChildChanged(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
+
+            }
+
+            @Override
+            public void onChildRemoved(@NonNull DataSnapshot dataSnapshot) {
+
+            }
+
+            @Override
+            public void onChildMoved(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+            // TODO: implement the ChildEventListener methods as documented above
+            // ...
+        });
         return customerList;
     }
 
     public void setCustomerList(List customerList) {
         this.customerList = customerList;
+        myRef.child("Customers").setValue(customerObject);
     }
 
     public void UpdateJsonStorage(){
+        // Write a message to the database
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference myRef = database.getReference("message");
 
+        myRef.setValue("Hello, World!");
     }
 
     //Hello guys!!
